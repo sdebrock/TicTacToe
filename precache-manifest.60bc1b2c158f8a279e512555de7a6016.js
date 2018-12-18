7bdec9ab30e41fc5cefc94a0a557f55fe6d73632
sdebrock <sdebrock@Shanes-MacBook-Pro.local>
2018-12-17 19:35:08 -0700

    Updates

---
+++ precache-manifest.60bc1b2c158f8a279e512555de7a6016.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ca3ec3df0cfade70a30",
+    "url": "/TicTacToe/static/css/main.5b677bc7.chunk.css"
+  },
+  {
+    "revision": "1ca3ec3df0cfade70a30",
+    "url": "/TicTacToe/static/js/main.1ca3ec3d.chunk.js"
+  },
+  {
+    "revision": "5bc363a705c279ce0d67",
+    "url": "/TicTacToe/static/js/1.5bc363a7.chunk.js"
+  },
+  {
+    "revision": "1a4430c086131fba29bb",
+    "url": "/TicTacToe/static/js/runtime~main.1a4430c0.js"
+  },
+  {
+    "revision": "51a0373187b73a2c183386b84ce3aa55",
+    "url": "/TicTacToe/index.html"
+  }
+];
